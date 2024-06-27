c3207fca2319a42e2bf57a671f68f0be85b80226
Cesar Gonzalez <cegor79@gmail.com>
2024-06-27 19:07:00 -0400

    Updated files employees_details.html and employees_details.js

--- EmployeeDetails/employee_details.js
+++ EmployeeDetails/employee_details.js
@@ -1,8 +1,28 @@
 const employees = [
-  { id: 1, name: "John Doe", age: 30, department: "IT", salary: 50000 },
-  { id: 2, name: "Alice Smith", age: 28, department: "HR", salary: 45000 },
-  { id: 3, name: "Bob Johnson", age: 35, department: "Finance", salary: 60000 },
-  //... More employee records can be added here
+  {
+    id: 1,
+    name: "John Doe",
+    age: 30,
+    department: "IT",
+    salary: 50000,
+    specialization: "Javascript",
+  },
+  {
+    id: 2,
+    name: "Alice Smith",
+    age: 28,
+    department: "HR",
+    salary: 45000,
+    specialization: "Typescript",
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    age: 35,
+    department: "Finance",
+    salary: 60000,
+    specialization: "Python",
+  },
 ];
 
 // Function to display all employees
@@ -10,7 +30,7 @@ function displayEmployees() {
   const totalEmployees = employees
     .map(
       (employee, index) =>
-        `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`
+        `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`
     )
     .join("");
   document.getElementById("employeesDetails").innerHTML = totalEmployees;
@@ -45,6 +65,20 @@ function findEmployeeById(employeeId) {
     document.getElementById(
       "employeesDetails"
     ).innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+  } else {
+    document.getElementById("employeesDetails").innerHTML =
+      "no employee has been found with this ID";
+  }
+}
+
+function findEmployeeBySpecialization(employeeSpecialization) {
+  const foundEmployee = employees.find(
+    (employee) => employee.specialization === employeeSpecialization
+  );
+  if (foundEmployee) {
+    document.getElementById(
+      "employeesDetails"
+    ).innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.specialization}</p>`;
   } else {
     document.getElementById("employeesDetails").innerHTML =
       "no employee has been found with this ID";
